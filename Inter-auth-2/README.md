e104f51936fd2319f93b5b654ad91a7867b6243c
Gautam-Hegde <gautamatjob@protonmail.com>
2023-09-09 14:23:16 +0530

    signup route

---
+++ Inter-auth-2/README.md
@@ -0,0 +1,3 @@
+### Description
+Functionally it is the same as Inter-auth, routes are the same as well.
+But storing the data in files instead of in-memory.
---
+++ Inter-auth-2/index.js
@@ -0,0 +1,108 @@
+const express=require('express');
+const app=express();
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+app.use(express.json());
+let ADMINS = [];
+let USERS = [];
+let COURSES = [];
+
+
+try {
+    ADMINS = JSON.parse(fs.readFileSync('admins.json', 'utf8'));
+    USERS = JSON.parse(fs.readFileSync('users.json', 'utf8'));
+    COURSES = JSON.parse(fs.readFileSync('courses.json', 'utf8'));
+} catch {
+    ADMINS = [];
+    USERS = [];
+    COURSES = [];
+}
+
+const secretKey='zoro';
+
+const generateJwt=(user)=>{
+    const payload = { username: user.username,}; //user.username = gautamhegde
+    return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+}
+const authenticateJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    //auth header is like bearer(whitespace)token 
+    if (authHeader) {
+      const token = authHeader.split(' ')[1];
+  
+      jwt.verify(token, secretKey, (err, user) => {
+        if (err) {
+          return res.sendStatus(403);
+        }
+        req.user = user;
+        console.log("inside middleware1: "+req.user);//[object object]
+        next();
+      });
+    } else {
+      res.sendStatus(401);//forbidden
+    }
+  };
+
+// admin routes signup login create update getall 
+app.post('/admin/signup',(req,res)=>{
+    const {username,password}=req.body;
+    const admin=ADMINS.find((a)=>{
+        return a.username===username;
+    })
+    if(admin){
+        res.status(403).json({message:'Admin already exists'});
+    }
+    else{
+        const newAdmin={username,password};
+        ADMINS.push(newAdmin);
+        fs.writeFileSync('admins.json',JSON.stringify(ADMINS));
+        const token = generateJwt(admin);
+        res.json({message:'Admin created successfully',token});
+    }
+
+});
+
+
+app.post('/admin/login',(req,res)=>{
+
+});
+
+
+app.post('/admin/courses',(req,res)=>{
+
+});
+
+app.put('/admin/courses/:courseId',(req,res)=>{
+
+});
+
+app.get('/admin/courses',(req,res)=>{
+
+});
+
+//user routes signup login getallcourse purchase getpurchased
+
+app.post('/user/signup',(req,res)=>{
+
+});
+
+
+app.post('/user/login',(req,res)=>{
+
+});
+
+app.get('/user/courses/',(req,res)=>{
+
+});
+
+app.post('/user/courses/:courseId',(req,res)=>{
+
+});
+app.get('/user/purchasedCourses',(req,res)=>{
+
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+  });
